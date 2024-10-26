24b33e9460f63e478aa0c44b68aa8821d1502b8d
Michael <200627081@student.georgianc.on.ca>
2024-10-26 12:56:00 -0400

    no animation

--- Assets/PlayerTrigger.cs
+++ Assets/PlayerTrigger.cs
@@ -14,24 +14,6 @@ public TMPro.TextMeshProUGUI titleText;
     {
         AreaTrigger area = collision.GetComponent<AreaTrigger>();
 
-        StopAllCoroutines();
-        StartCoroutine(AnimateTitle(area, 5));
-    }
-     public IEnumerator AnimateTitle(AreaTrigger area, float duration)
-    {
-        titleText.text = area.title;
-        byLineText.text = "By: " + area.author;
-
-        float timeElapsed = 0;
-        while(timeElapsed < duration)
-        {
-            float percentage = timeElapsed / duration;
-            titleText.alpha = 1.0f - percentage;
-            byLineText.alpha = 1.0f - percentage;
-            yield return new WaitForEndOfFrame();
-            timeElapsed += Time.deltaTime;
-        }
-
         titleText.text = "";
         byLineText.text = "";
     }
