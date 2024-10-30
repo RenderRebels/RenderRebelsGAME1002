a0ffffad7b5cf7d6c9a96a37e0eb9ba5e8b045ac
Michael <200627081@student.georgianc.on.ca>
2024-10-29 20:47:11 -0400

    Redid code pretty much

--- Assets/PlayerTrigger.cs
+++ Assets/PlayerTrigger.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 using TMPro;
 
 public class PlayerTrigger : MonoBehaviour
+
 {
     // Start is called before the first frame update
 public TMPro.TextMeshProUGUI titleText;
-    public TMPro.TextMeshProUGUI byLineText;
+ public TMPro.TextMeshProUGUI byLineText;
+ public void OnTriggerEnter2D(Collider2D collision)
+ {
+    AreaTrigger area = collision.GetComponent<AreaTrigger>();
+    titleText.text = area.title;
+    byLineText.text = "By: " + area.author;
 
+ }
 
-   public void OnTriggerEnter2D(Collider2D collision)
-    {
-        AreaTrigger area = collision.GetComponent<AreaTrigger>();
-
-        titleText.text = "";
-        byLineText.text = "";
-    }
 }
