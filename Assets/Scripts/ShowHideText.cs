78fefff848ee1303e2328ba2e9a6d14148568f96
Michael <200627081@student.georgianc.on.ca>
2024-11-23 13:24:38 -0500

    made text disappear after 5 sec

--- Assets/Scripts/ShowHideText.cs
+++ Assets/Scripts/ShowHideText.cs
@@ -1,14 +1,16 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class ShowHideText : MonoBehaviour
 {
-    public TextMeshProUGUI textMeshPro; 
-    private bool isTextVisible = false;  
+    public TextMeshProUGUI textMeshPro;
+    private bool isTextVisible = false;
+    private float timer = 5f; // Timer duration in seconds
+    private bool timerRunning = false; // Flag to track if the timer is running
 
     void Start()
     {
-  
         textMeshPro.gameObject.SetActive(false);
     }
 
@@ -16,11 +18,24 @@ public class ShowHideText : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-         
+            // Toggle visibility of the text
             isTextVisible = !isTextVisible;
-
-          
             textMeshPro.gameObject.SetActive(isTextVisible);
+
+            // Start the timer if the text is shown
+            if (isTextVisible && !timerRunning)
+            {
+                StartCoroutine(HideTextAfterTime());
+            }
         }
+    }
+
+    private IEnumerator HideTextAfterTime()
+    {
+        timerRunning = true; // Set flag to true to indicate timer is running
+        yield return new WaitForSeconds(timer); // Wait for the specified time
+        textMeshPro.gameObject.SetActive(false); // Hide the text
+        isTextVisible = false; // Update the visibility state
+        timerRunning = false; // Reset the timer running flag
     }
 }
